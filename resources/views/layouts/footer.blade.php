bc0216856f53c62224d436450d47c3198f947a61
YegorChevardin <egor03052004@gmail.com>
2020-07-06 15:36:43 +0300

    version 0.0002

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,72 @@
+<footer class="footer-area three">
+    <div class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-widget">
+                        <h3>why teachhouse</h3>
+                        <ul>
+                            <li><a href="#">great campus</a></li>
+                            <li><a href="#">various faculty</a></li>
+                            <li><a href="#">low cost</a></li>
+                            <li><a href="#">transport support</a></li>
+                            <li><a href="#">nice hostel</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-widget">
+                        <h3>information</h3>
+                        <ul>
+                            <li><a href="#">addmission</a></li>
+                            <li><a href="#">Academic Calender</a></li>
+                            <li><a href="{{ route('events') }}">Event List</a></li>
+                            <li><a href="#">Hostel &amp; Dinning</a></li>
+                            <li><a href="#">TimeTable</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-widget">
+                        <h3>useful links</h3>
+                        <ul>
+                            <li><a href="{{ route('courses') }}">our courses</a></li>
+                            <li><a href="{{ route('about') }}">about us</a></li>
+                            <li><a href="{{ route('teachers') }}">teachers &amp; faculty</a></li>
+                            <li><a href="#">teams &amp; conditions</a></li>
+                            <li><a href="{{ route('events') }}">our events</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-widget">
+                        <h3>get in touch</h3>
+                        <p>Your address goes here, Street<br>City, Roadno 785 New York</p>
+                        <p>+380999425174</p>
+                        <p>besfor16@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom two text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="footer-logo text-center">
+                        <a href="{{ route('/') }}"><img src="{{ asset('assets/img/logo/footer-logo.png') }}" alt="teachhouse"></a>
+                    </div>
+                    <p>Copyright © <a href="#">TeachHouse</a> 2017. All Right Reserved.</p>
+                    <div class="footer-social">
+                        <ul>
+                            <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-pinterest"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-vimeo"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('/');
+Route::get('/about', function() {
+    return view('about');
+})->name('about');
+Route::get('/courses', function() {
+    return view('courses');
+})->name('courses');
+Route::get('/events', function() {
+    return view('events');
+})->name('events');
+Route::get('/teachers', function() {
+    return view('teachers');
+})->name('teachers');
+Route::get('/teacher-details', function() {
+    return view('teacher-details');
+})->name('teacher-details');
+Route::get('/blog', function() {
+    return view('blog');
+})->name('blog');
+Route::get('/contact', function() {
+    return view('contact');
+})->name('contact');
+Route::get('/course-details', function() {
+    return view('course-details');
+})->name('course-details');
+Route::get('/event-details', function() {
+    return view('event-details');
+})->name('event-details');
+Route::get('/blog-details', function() {
+    return view('blog-details');
+})->name('blog-details');
+
+/* Auth rotes section start */
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/login', function() {
+    return view('auth.login');
+})->name('login');
+Route::get('/register', function() {
+    return view('auth.register');
+})->name('register');
+/* Auth rotes section end */
